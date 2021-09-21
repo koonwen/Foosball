776601681b4e41f9e9d110f785a30158f80bb50d
Bernard Boey <bernard@u.yale-nus.edu.sg>
2021-09-21 12:21:15 +0800

    Implement skeletal version of MainActivity (main menu screen)

--- app/src/main/java/com/example/foosball/MainActivity.java
+++ app/src/main/java/com/example/foosball/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.foosball;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FullScreenActivity {
+
+    private Button createGame;
+    private Button joinGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createGame = findViewById(R.id.createGame);
+        joinGame = findViewById(R.id.joinGame);
+        createGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LobbyActivity.class);
+                startActivity(intent);
+            }
+        });
+        joinGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LobbyActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
