702180cdfe8a30052340f0cbf03b6d7e39ed6e61
Bernard Boey <bernard@u.yale-nus.edu.sg>
2021-10-03 12:16:45 +0800

    Add connection to Firebase Cloud Firestore

---
+++ app/src/main/java/com/example/foosball/Database.java
@@ -0,0 +1,37 @@
+package com.example.foosball;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
+public class Database {
+    public static final String TAG = "Database";
+
+    public static void createGame(String playerName) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        HashMap<String, Object> game = new HashMap<>();
+        game.put("player1", playerName);
+        Task<DocumentReference> task = db.collection("games")
+                .add(game)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+}
