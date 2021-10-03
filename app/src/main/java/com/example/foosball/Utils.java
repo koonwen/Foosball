a7bba0843013ca54bd79662c6655fd80ee4c9c4f
Tristan Koh <tklw.14@gmail.com>
2021-10-03 12:09:37 +0800

    add functionality to save game code in menu screens

--- app/src/main/java/com/example/foosball/Utils.java
+++ app/src/main/java/com/example/foosball/Utils.java
@@ -6,6 +6,7 @@ import android.preference.PreferenceManager;
 
 public class Utils {
     private static final String preferencePlayerName = "playerName";
+    private static final String preferenceGameCode = "gameCode";
 
     public static void setPlayerName(Context context, String playerName) {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
@@ -17,5 +18,17 @@ public class Utils {
     public static String getPlayerName(Context context) {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
         return preferences.getString(preferencePlayerName, "");
+    }
+
+    public static void setGameCode(Context context, String gameCode) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(preferenceGameCode, gameCode);
+        editor.commit();
+    }
+
+    public static String getGameCode(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getString(preferenceGameCode, "");
     }
 }
