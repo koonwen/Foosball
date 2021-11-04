42ac4c0644ba98d6f64de9cff08f06d9ef8f0815
Bernard Boey <bernard@u.yale-nus.edu.sg>
2021-11-05 03:21:28 +0800

    Clean up Utils class

--- app/src/main/java/com/example/foosball/utils/Utils.java
+++ app/src/main/java/com/example/foosball/utils/Utils.java
@@ -1,55 +1,38 @@
-package com.example.foosball;
+package com.example.foosball.utils;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+/**
+ * Provides utility methods.
+ */
 public class Utils {
-    private static final String PREFERENCE_PLAYER_NAME = "playerName";
     private static final String PREFERENCE_PLAYER_ID = "playerId";
     private static final String PREFERENCE_GAME_CODE = "gameCode";
     public static final int NUM_CHARS_GAME_CODE = 6;
     public static final int HOST_PLAYER_ID = 1;
 
     /**
-     * Saves user entered player name into the Editor, so that it can be accessed by other activities
-     * @param context
-     * @param playerName User entered player name
+     * Saves current player ID so that it can be accessed by other activities.
+     *
+     * @param context Application context.
+     * @param playerId Current player ID.
      */
-    public static void setPlayerName(Context context, String playerName) {
-        final SharedPreferences preferences =
-                PreferenceManager.getDefaultSharedPreferences(context);
-        final SharedPreferences.Editor editor = preferences.edit();
-        editor.putString(PREFERENCE_PLAYER_NAME, playerName);
-        editor.commit();
-    }
-
-    public static String getPlayerName(Context context) {
-        final SharedPreferences preferences =
-                PreferenceManager.getDefaultSharedPreferences(context);
-        return preferences.getString(PREFERENCE_PLAYER_NAME, "");
-    }
-
-    /**
-     * Saves current player ID into the Editor, so that it can be accessed by other activities
-     * @param context
-     * @param playerId Current player ID
-     */
-
     public static void setPlayerId(Context context, int playerId) {
         final SharedPreferences preferences =
                 PreferenceManager.getDefaultSharedPreferences(context);
         final SharedPreferences.Editor editor = preferences.edit();
         editor.putInt(PREFERENCE_PLAYER_ID, playerId);
-        editor.commit();
+        editor.apply();
     }
 
     /**
-     * Returns the previously saved player ID
-     * @param context
-     * @return Integer player ID
+     * Returns the previously saved player ID.
+     *
+     * @param context Application context.
+     * @return Integer player ID.
      */
-
     public static int getPlayerId(Context context) {
         final SharedPreferences preferences =
                 PreferenceManager.getDefaultSharedPreferences(context);
@@ -57,35 +40,35 @@ public class Utils {
     }
 
     /**
-     * Boolean for whether the current player is the game host based on player ID
-     * @param context
-     * @return Boolean
+     * Checks whether the current player is the game host based on player ID.
+     *
+     * @param context Application context.
+     * @return True if the current player is the game host; false otherwise.
      */
-
     public static boolean isGameHost(Context context) {
         return getPlayerId(context) == HOST_PLAYER_ID;
     }
 
     /**
-     * Saves the current game code into the Editor so that it can be accessed by other activities
-     * @param context
-     * @param gameCode String current game code
+     * Saves the current game code so that it can be accessed by other activities
+     *
+     * @param context Application context.
+     * @param gameCode Game code.
      */
-
     public static void setGameCode(Context context, String gameCode) {
         final SharedPreferences preferences =
                 PreferenceManager.getDefaultSharedPreferences(context);
         final SharedPreferences.Editor editor = preferences.edit();
         editor.putString(PREFERENCE_GAME_CODE, gameCode);
-        editor.commit();
+        editor.apply();
     }
 
     /**
-     * Returns the previously saved game code
-     * @param context
-     * @return String game code
+     * Returns the previously saved game code.
+     *
+     * @param context Application context.
+     * @return Game code.
      */
-
     public static String getGameCode(Context context) {
         final SharedPreferences preferences =
                 PreferenceManager.getDefaultSharedPreferences(context);
@@ -93,10 +76,10 @@ public class Utils {
     }
 
     /**
-     * Random generator for a 6 character game code
-     * @return String gamecode
+     * Returns a random game code according to the specified requirements.
+     *
+     * @return Generated game code.
      */
-
     public static String generateGameCode() {
         final String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         final StringBuilder stringBuilder = new StringBuilder();
