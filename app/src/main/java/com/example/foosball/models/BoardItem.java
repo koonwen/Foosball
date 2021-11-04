4f9ab1fbc2e0ec648f09854684ed9e477fb36697
Bernard Boey <bernard@u.yale-nus.edu.sg>
2021-11-05 04:48:22 +0800

    Clean up BoardItem class

--- app/src/main/java/com/example/foosball/models/BoardItem.java
+++ app/src/main/java/com/example/foosball/models/BoardItem.java
@@ -1,12 +1,13 @@
 package com.example.foosball.models;
 
-import android.graphics.Bitmap;
-import android.graphics.Rect;
-
 interface BoardItem {
-    public void setPoint(int X, int Y);
-    public int getPointX();
-    public int getPointY();
-    public int getWidth();
-    public int getHeight();
+    void setPoint(int X, int Y);
+
+    int getPointX();
+
+    int getPointY();
+
+    int getWidth();
+
+    int getHeight();
 }
