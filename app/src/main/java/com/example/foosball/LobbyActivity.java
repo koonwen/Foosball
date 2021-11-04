66fd2cdb8b2c22826f75bbf3c12a126d63b8a632
Bernard Boey <bernard@u.yale-nus.edu.sg>
2021-11-05 03:25:36 +0800

    Clean up LobbyActivity class

--- app/src/main/java/com/example/foosball/LobbyActivity.java
+++ app/src/main/java/com/example/foosball/LobbyActivity.java
@@ -2,6 +2,7 @@ package com.example.foosball;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -9,14 +10,15 @@ import android.widget.TextView;
 import com.example.foosball.database.BasicDatabaseListener;
 import com.example.foosball.database.Database;
 import com.example.foosball.database.GameStatusListener;
+import com.example.foosball.utils.Utils;
 
 import java.util.ArrayList;
 
 /**
- * Initialises the lobby screen
+ * Represents the game lobby screen.
  */
-
 public class LobbyActivity extends FullScreenActivity {
+    private static final String TAG = "LobbyActivity";
 
     /**
      * Stores the player 1 - 2 text views in an array list
@@ -34,11 +36,8 @@ public class LobbyActivity extends FullScreenActivity {
 
     /**
      * Initialises the logic of the buttons and pulls data from the db upon creation of the
-     * lobby activity
-     *
-     * @param savedInstanceState
+     * lobby activity.
      */
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,31 +49,24 @@ public class LobbyActivity extends FullScreenActivity {
 
         final String gameCode = Utils.getGameCode(getApplicationContext());
         final TextView gameCodeText = findViewById(R.id.codeID);
+        gameCodeText.setText(gameCode);
         final String placeholderPlayerName =
                 getResources().getString(R.string.placeholder_player_name);
-        gameCodeText.setText(gameCode);
 
         final Database database = Database.getInstance();
-        /**
-         * Starts the game status listener to constantly update the player names and status of the game
-         */
-        database.startGameStatusListener(new GameStatusListener() {
 
-            /**
-             * Method is called when the game status listener successfully fetches data from the db
-             *
-             * Updates the names of the players and sets the visibility of the start game button
-             * depending on whether the current player is player1 "i.e. host"
-             *
-             * @param playerNames List of player names.
-             * @param twoPlayers Whether there is an even number of players.
-             * @param gameStarted Whether the game has started.
-             * @param gameEnded Whether the game has ended.
-             */
+        // Starts the game status listener to constantly update the player names and game status.
+        // Updates the names of the players and sets the visibility of the start game button
+        // depending on whether the current player is player1 "i.e. host".
+        database.startGameStatusListener(new GameStatusListener() {
             @Override
             public void onSuccess(ArrayList<String> playerNames, Boolean twoPlayers,
                                   Boolean gameStarted, Boolean gameEnded) {
+                Log.d(TAG, "Game status update - Players: " + playerNames + ", gameStarted: "
+                        + gameStarted + ", gameEnded: " + gameEnded);
+
                 ArrayList<TextView> playerTextViews = getPlayerTextViews();
+                assert playerTextViews.size() == playerNames.size();
 
                 for (int i = 0; i < playerTextViews.size(); i++) {
                     final TextView playerTextView = playerTextViews.get(i);
@@ -86,70 +78,68 @@ public class LobbyActivity extends FullScreenActivity {
                     }
                 }
 
-                // If there are 2 players and host, then make start button visible
-                if (Utils.isGameHost(getApplicationContext()) && twoPlayers
-                        && !gameStarted && !gameEnded) {
+                // Automatically start game once host has started game
+                if (gameStarted) {
+                    Log.i(TAG, "Starting game... moving to game screen");
+                    final Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+                    database.stopGameStatusListener();
+                    startActivity(intent);
+                }
+
+                // Make start button visible for the host if there are two players
+                if (Utils.isGameHost(getApplicationContext()) && twoPlayers) {
                     startGameButton.setVisibility(View.VISIBLE);
                 } else {
                     startGameButton.setVisibility(View.GONE);
                 }
-                startGameButton.setVisibility(View.VISIBLE);
-
-                // Automatically start game for rest of players once host has started game
-                if (!Utils.isGameHost(getApplicationContext()) && gameStarted && !gameEnded) {
-                    final Intent intent = new Intent(getApplicationContext(),
-                            GameActivity.class);
-                    database.stopGameStatusListener();
-                    startActivity(intent);
-                }
+                // TODO: Remove line before production. Uncomment for testing game without needing
+                // a second player.
+                // startGameButton.setVisibility(View.VISIBLE);
             }
 
             @Override
             public void onConnectionError() {
+                Log.e(TAG, "Connection error while listening to game status");
             }
         });
 
-
         final int playerId = Utils.getPlayerId(getApplicationContext());
-
         final Button returnMenu = findViewById(R.id.returnMenu);
 
-        /**
-         * Removes the current player when back button is clicked and starts the main menu activity
-         */
-        returnMenu.setOnClickListener(view ->
-                database.removePlayer(playerId, new BasicDatabaseListener() {
-                    @Override
-                    public void onSuccess() {
-                        final Intent intent = new Intent(getApplicationContext(),
-                                MainActivity.class);
-                        startActivity(intent);
-                    }
-
-                    @Override
-                    public void onConnectionError() {
-                    }
-                })
-        );
-
-        /**
-         * Updates the status of the game on the db and starts the game activity when start game
-         * button is clicked
-         */
-        startGameButton.setOnClickListener(view -> {
-            database.updateStartGame(new BasicDatabaseListener() {
+        // Removes the current player when back button is clicked and starts the main menu activity.
+        returnMenu.setOnClickListener(view -> {
+            Log.i(TAG, "Trying to exit lobby");
+            database.removePlayer(playerId, new BasicDatabaseListener() {
                 @Override
                 public void onSuccess() {
+                    Log.i(TAG, "Moving back to main menu screen");
+                    final Intent intent = new Intent(getApplicationContext(),
+                            MainActivity.class);
+                    startActivity(intent);
                 }
 
                 @Override
                 public void onConnectionError() {
+                    Log.e(TAG, "Connection error while trying to exit lobby");
                 }
             });
-            database.stopGameStatusListener();
-            final Intent intent = new Intent(getApplicationContext(), GameActivity.class);
-            startActivity(intent);
         });
 
+        // Updates the status of the game on the db and starts the game activity when start game
+        // button is clicked.
+        startGameButton.setOnClickListener(view -> {
+            Log.i(TAG, "Trying to start game");
+            database.updateStartGame(new BasicDatabaseListener() {
+                @Override
+                public void onSuccess() {
+                    Log.i(TAG, "Successfully started game");
+                }
+
+                @Override
+                public void onConnectionError() {
+                    Log.e(TAG, "Connection error while trying to start game");
+                }
+            });
+        });
     }
 }
