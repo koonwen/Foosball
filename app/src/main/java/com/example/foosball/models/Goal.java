22e090617f26a9ab2e2ae3b852116ed5ec028793
Bernard Boey <bernard@u.yale-nus.edu.sg>
2021-11-06 00:02:57 +0800

    Clean up Goal class

--- app/src/main/java/com/example/foosball/models/Goal.java
+++ app/src/main/java/com/example/foosball/models/Goal.java
@@ -3,10 +3,6 @@ package com.example.foosball.models;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.view.View;
-
-import com.google.android.material.snackbar.BaseTransientBottomBar;
-import com.google.android.material.snackbar.Snackbar;
 
 /**
  * Handles the logic for the goals.
@@ -15,12 +11,13 @@ public class Goal {
     private int topY;
     private int bottomY;
     private int xPos;
-    private String name;
-    private Paint paint = new Paint();
+    private final String name;
+    private final Paint paint = new Paint();
     private int conceeded = 0;
 
     /**
      * Goal contructor. Goal name is the team name's goal
+     *
      * @param name
      */
     public Goal(String name) {
@@ -31,8 +28,9 @@ public class Goal {
 
     /**
      * Setter for goals location.
+     *
      * @param x the x coordinate of the goal. Team A's x-coord goal should be 0 and Team B's
-     *          x-coord at canvasWidth
+     *         x-coord at canvasWidth
      * @param top the maxY of the goal region
      * @param bottom the minY of the goal region
      */
@@ -45,20 +43,24 @@ public class Goal {
     /**
      * Function to check if the ball has been scored. Updates the class's goal conceeded count
      * and shows a popup of the goal scored.
+     *
      * @param y
-     * @param view
      */
-    public boolean checkGoal(int y, View view) {
+    public boolean checkGoal(int y) {
         if (y < topY && y > bottomY) {
-            conceeded += 1;
-            Snackbar.make(view, "GOAL!!! " + name + " conceeded " + conceeded + " goals", BaseTransientBottomBar.LENGTH_LONG).show();
+            scoreGoal();
             return true;
         }
         return false;
     }
 
+    public void scoreGoal() {
+        conceeded += 1;
+    }
+
     /**
      * To be used by the gameboard to redraw the goal at every iteration.
+     *
      * @param canvas
      */
     public void refresh(Canvas canvas) {
@@ -67,8 +69,11 @@ public class Goal {
 
     /**
      * Returns the number of goals conceeded
+     *
      * @return
      */
-    public int getConceeded() {return conceeded;}
+    public int getConceeded() {
+        return conceeded;
+    }
 
 }
